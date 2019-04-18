a3db16986831f961c769df42d671255eafa17f70
akash <akashsinghk02@gmail.com>
2019-04-18 06:38:58 +0530

    Added React-Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,65 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { addRecipi, removeFromcalendar } from '../actions'
+import { connect } from 'react-redux'
+import { addRecipi, removeFromCalender } from '../actions'
+
 
 class App extends Component {
-  state = {
-    calendar: null
+
+  handleThings = () => {
+    this.props.dispatch(addRecipi({}))
   }
 
-  componentDidMount() {
-    const { store } = this.props;
-
-    store.subscribe(() => {
-
-      this.setState(() => ({
-        calendar: store.getState()
-      })
-      )
-
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipi({
-      day: 'monday',
-      meal: 'breakfast',
-      recipi: {
-        label: this.input.value
-      }
-
-    }));
-
-    this.input.value = '';
-  }
-
-  input = "Aakash Singh"
   render() {
+    console.log('props =', this.props);
     return (
-      <div className="App">
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        >
-        </input>
-        <button onClick={this.submitFood}>submit</button>
-
-        <pre>
-          Mondays's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+      <div >
+        Hello World
       </div>
     );
   }
 }
 
-export default App;
+
+function mapStateToProps(calender) {
+  const dayOrder = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+  return {
+    calender: dayOrder.map((day) => (
+      {
+        day,
+        meals: Object.keys(calender[day]).reduce((meals, meal) => {
+          meals[meal] = calender[day][meal] ?
+            calender[day][meal] :
+            null
+
+          return meals;
+
+        }, {})
+      }
+    ))
+  }
+}
+
+
+
+/**  Alternative Logic */
+
+// function mapStateToProps(calender) {
+//   const dayOrder = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+//   return {
+//     calender: dayOrder.map((day) => (
+//       {
+//         day,
+//         meals: calender[day] ?
+//           calender[day] :
+//           {
+//             breakfast: null,
+//             dinner: null,
+//             lunch: null
+//           }
+//       }
+//     ))
+//   }
+// }
+
+export default connect(mapStateToProps)(App);
